2a750dec34bf332d1838ef9e787435a8bf5ba6bf
Michael-Bill <biloshitskiy_m@mail.ru>
2024-03-09 16:13:44 +0300

    Первая версия проекта с авторизацией

---
+++ backend/avito-prices-api/src/main/java/org/bigbrainmm/avitopricesapi/controller/AuthController.java
@@ -0,0 +1,34 @@
+package org.bigbrainmm.avitopricesapi.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.bigbrainmm.avitopricesapi.dto.JwtAuthenticationResponse;
+import org.bigbrainmm.avitopricesapi.dto.SignInRequest;
+import org.bigbrainmm.avitopricesapi.dto.SignUpRequest;
+import org.bigbrainmm.avitopricesapi.service.AuthenticationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Регистрация пользователя")
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @Operation(summary = "Авторизация пользователя")
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
---
+++ backend/avito-prices-api/src/main/java/org/bigbrainmm/avitopricesapi/service/AuthenticationService.java
@@ -0,0 +1,61 @@
+package org.bigbrainmm.avitopricesapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.bigbrainmm.avitopricesapi.dto.JwtAuthenticationResponse;
+import org.bigbrainmm.avitopricesapi.dto.SignInRequest;
+import org.bigbrainmm.avitopricesapi.dto.SignUpRequest;
+import org.bigbrainmm.avitopricesapi.model.Role;
+import org.bigbrainmm.avitopricesapi.model.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    /**
+     * Регистрация пользователя
+     *
+     * @param request данные пользователя
+     * @return токен
+     */
+    public JwtAuthenticationResponse signUp(SignUpRequest request) {
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ROLE_USER)
+                .build();
+
+        userService.create(user);
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    /**
+     * Аутентификация пользователя
+     *
+     * @param request данные пользователя
+     * @return токен
+     */
+    public JwtAuthenticationResponse signIn(SignInRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword()
+        ));
+
+        var user = userService
+                .userDetailsService()
+                .loadUserByUsername(request.getUsername());
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+}
