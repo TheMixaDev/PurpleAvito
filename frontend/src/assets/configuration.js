cc723668637f962952a23706cff3301a979e15fd
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-12 01:16:44 +0300

    Adapt for nginx

--- frontend/src/assets/configuration.js
+++ frontend/src/assets/configuration.js
@@ -1,5 +1,5 @@
 export const configuration = {
-    serverUrl: import.meta.env.MODE === 'development' ? `http://localhost:8080/api/` : '/api/v1/',
+    serverUrl: import.meta.env.MODE === 'development' ? `http://localhost/api/` : '/api/',
     debugMode: true,
     settingsUpdate: 60 * 1000,
     minLoadingTimer: 200 // Increase if animations looks too fast, set to 0 to remove fake ping
