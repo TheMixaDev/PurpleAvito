453b5f30fa49e7cca68196066c0ff276395e213a
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-11 21:40:57 +0300

    fix replica pricepool

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 docker rmi -f purpleavitoback-avito_admin_api
-docker rmi -f purpleavitoback-avito_prices_api
+docker rmi -f purpleavitoback-avito_prices_api_primary
+docker rmi -f purpleavitoback-avito_prices_api_replica_1
 docker-compose up
