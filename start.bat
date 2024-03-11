39af1ec6ce5cd3d06ec12f65007241c243a081e8
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-11 15:11:49 +0300

    Increase max file size

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
-docker-compose rm -f
-docker rmi purpleavitoback-avito_admin_api
-docker rmi purpleavitoback-avito_prices_api
+docker rmi -f purpleavitoback-avito_admin_api
+docker rmi -f purpleavitoback-avito_prices_api
 docker-compose up
