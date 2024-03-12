311fccf554693b7b80044d3995da8e631477ea80
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-12 15:55:26 +0300

    Updated compose and start

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 docker rmi -f purpleavitoback-avito-admin-api
 docker rmi -f purpleavitoback-avito-prices-api-primary
 docker rmi -f purpleavitoback-avito-prices-api-replica-1
+docker rmi -f purpleavitoback-avito-prices-api-replica-2
 docker-compose up --build
