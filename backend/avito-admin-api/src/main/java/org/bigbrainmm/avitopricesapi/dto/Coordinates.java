8ef23e6127c6585334927cc7869b6b358b18d0f3
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-12 15:58:41 +0300

    Coordinates and names for services

---
+++ backend/avito-admin-api/src/main/java/org/bigbrainmm/avitopricesapi/dto/Coordinates.java
@@ -0,0 +1,17 @@
+package org.bigbrainmm.avitopricesapi.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Schema(description = "Координаты")
+public class Coordinates {
+    double lat;
+    double lon;
+}
