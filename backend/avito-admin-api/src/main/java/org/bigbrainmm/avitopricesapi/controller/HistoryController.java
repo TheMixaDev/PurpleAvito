664939c22572f4714bf11c5dc772113c50d69c6e
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-12 22:53:52 +0300

    Add pagination to history

--- backend/avito-admin-api/src/main/java/org/bigbrainmm/avitopricesapi/controller/HistoryController.java
+++ backend/avito-admin-api/src/main/java/org/bigbrainmm/avitopricesapi/controller/HistoryController.java
@@ -2,10 +2,14 @@ package org.bigbrainmm.avitopricesapi.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.bigbrainmm.avitopricesapi.dto.HistoryResponse;
 import org.bigbrainmm.avitopricesapi.entity.History;
 import org.bigbrainmm.avitopricesapi.repository.HistoryRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +21,13 @@ import java.util.List;
 public class HistoryController {
     private final HistoryRepository historyRepository;
     @GetMapping
-    public List<History> getHistory() {
-        return historyRepository.findAll();
+    public HistoryResponse getHistory(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int limit
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, limit, Sort.by(Sort.Direction.DESC, "timestamp"));
+        List<History> history = historyRepository.findAll(pageRequest).getContent();
+        long total = historyRepository.count();
+        return new HistoryResponse(history, total);
     }
 }
