bd0f1811216f5f0ed1ea8ac3ffc3d5aaee6c50c4
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-03-14 06:10:11 +0300

    fixes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-docker-compose up postgres-primary -d
-docker-compose up avito-admin-api -d
+docker-compose up -d postgres-primary
+docker-compose up -d avito-admin-api
 
 sleep 10
 
@@ -8,15 +8,19 @@ docker-compose restart avito-admin-api
 
 sleep 10
 
-docker-compose up avito-prices-api-primary -d
+docker-compose up -d avito-prices-api-primary
 
 sleep 10
 
-docker-compose up postgres-replica-1 -d
-docker-compose up postgres-replica-2 -d
+docker-compose up -d postgres-replica-1
+docker-compose up -d postgres-replica-2
 
 sleep 10
 
-docker-compose up avito-prices-api-replica-1 -d
-docker-compose up avito-prices-api-replica-2 -d
-docker-compose up --build nginx -d
+docker-compose up -d avito-prices-api-replica-1
+docker-compose up -d avito-prices-api-replica-2
+docker-compose up -d --build frontend
+
+sleep 5
+
+docker-compose up --build -d nginx
